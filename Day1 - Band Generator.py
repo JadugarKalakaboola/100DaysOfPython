9a8ddbe5c4b57dbd111230238cefa658eed55f67
Siddharth <sid.sharma1704@gmail.com>
2022-06-30 11:57:42 +0530

    Day 1

---
+++ Day1 - Band Generator.py
@@ -0,0 +1,7 @@
+city_name = input("What's name of the city you grew up in? ")
+
+pet_name = input("What is your pets name? ")
+
+band_name = city_name + " " + pet_name
+
+print("Your band name could be", band_name)
